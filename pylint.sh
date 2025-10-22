6ecb2db5ed8108f229caeae411f350f4cd948a68
Diederick de Buck <diederickdebuck@liberator.home>
2025-10-22 23:59:10 +0200

    dddd

--- pylint.sh
+++ pylint.sh
@@ -1,4 +1,11 @@
+
 pylint plugins/modules/graph_reset.py
 pylint plugins/modules/query_read.py
 pylint plugins/modules/vertex.py
 pylint plugins/modules/edge.py
+
+PYTHONPATH=./plugins/module_utils 
+pylint argument_spec.py
+pylint cypher_query.py
+pylint schema.py
+pylint skeleton.py
