611bb0ca72cf7b71dc8aa2d392bec7e2a82c397a
Diederick de Buck <diederickdebuck@liberator.home>
2025-10-23 15:22:30 +0200

    dddd

--- plugins/modules/query_read.py
+++ plugins/modules/query_read.py
@@ -80,7 +80,7 @@ def validate_cypher_inputs(
     # validate edge properties against injection via JSON-key
     for key in module_params[u_skel.JsonTKN.PARAMETERS.value].keys():
         result, diagnostics = u_schema.validate_pattern(
-            u_schema.SchemaProperties.PROPERTY_KEYS.value,
+            u_schema.SchemaProperties.PROPERTY_KEYS,
             key
         )
         if not result:
