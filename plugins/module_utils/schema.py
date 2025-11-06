6719c0bf360f8b1e6ed43dc2a0aa995e05e332e8
Diederick de Buck <diederickdebuck@liberator.home>
2025-11-06 15:25:55 +0100

    dddd

--- plugins/module_utils/schema.py
+++ plugins/module_utils/schema.py
@@ -1,5 +1,4 @@
 # pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,
-# pylint: disable=line-too-long,too-many-arguments,too-many-locals
 """
     Filename: ./module_utils/schema.py
     Author: diederick de Buck (diederick.de.buck@gmail.com)
