dac5cf9c8d6f2b593dbcadb6c9438c3471abcbf2
Diederick de Buck <diederickdebuck@liberator.home>
2025-11-12 10:30:36 +0100

    ddd

--- pylint.sh
+++ pylint.sh
@@ -5,15 +5,14 @@ pylint plugins/modules/vertex.py
 pylint plugins/modules/edge.py
 
 PYTHONPATH=./plugins/module_utils
-PYLINT_INIT_HOOK=--init-hook "import sys; sys.path.append('./plugins')
 
-cd $PYTHONPATH
-pylint --init-hook "import sys; sys.path.append('./plugins')" argument_spec.py
-pylint --init-hook "import sys; sys.path.append('./plugins')" cypher_query.py
-pylint --init-hook "import sys; sys.path.append('./plugins')" cypher.py
-pylint --init-hook "import sys; sys.path.append('./plugins')" driver
-pylint --init-hook "import sys; sys.path.append('./plugins')" input.py
-pylint --init-hook "import sys; sys.path.append('./plugins')" properties.py
-pylint --init-hook "import sys; sys.path.append('./plugins')" schema.py
-pylint --init-hook "import sys; sys.path.append('./plugins')" shared.py
-pylint --init-hook "import sys; sys.path.append('./plugins')" skeleton.py
+cd ${PYTHONPATH}
+pylint argument_spec.py
+pylint cypher_query.py
+pylint cypher.py
+pylint driver
+pylint input.py
+pylint properties.py
+pylint schema.py
+pylint shared.py
+pylint skeleton.py
